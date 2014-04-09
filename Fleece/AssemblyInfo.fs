87c66e428aeb03f53293653b668abbce28fcece3
mausch <mauricioscheffer@gmail.com>
2014-04-09 12:33:02 -0300

    Version 0.2

--- Fleece/AssemblyInfo.fs
+++ Fleece/AssemblyInfo.fs
@@ -12,7 +12,7 @@ open System.Reflection
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
-[<assembly: AssemblyVersion("0.1.0.0")>]
-[<assembly: AssemblyFileVersion("0.1.0.0")>]
-[<assembly: AssemblyInformationalVersion("0.1.0.0")>]
+[<assembly: AssemblyVersion("0.2.0.0")>]
+[<assembly: AssemblyFileVersion("0.2.0.0")>]
+[<assembly: AssemblyInformationalVersion("0.2.0.0")>]
 do()
