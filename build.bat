1e032cd4a8a5731b9e4d2dde65804774fbb31b60
mausch <mauricioscheffer@gmail.com>
2014-09-08 16:37:34 -0300

    Fix build config

--- build.bat
+++ build.bat
@@ -6,4 +6,13 @@ if %PROCESSOR_ARCHITECTURE%==x86 (
 ) else ( set MSBUILD="%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 )
 if not exist .nuget\nuget.exe %MSBUILD% .nuget\nuget.targets /t:CheckPrerequisites
-%MSBUILD% /m /p:Configuration=Release Fleece.sln
+%MSBUILD% /m /p:Configuration=SystemJson Fleece.sln
+if ERRORLEVEL 1 (
+	echo Error building Fleece for System.Json
+	exit /b 1
+)
+%MSBUILD% /m /p:Configuration=FSharpData Fleece.sln
+if ERRORLEVEL 1 (
+	echo Error building Fleece for FSharp.Data
+	exit /b 2
+)
