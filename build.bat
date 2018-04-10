15cccf6bb626d659e50e29ca6f1bdf9cd57dbfcc
Oskar Gewalli <oskar@gewalli.se>
2018-04-10 19:45:07 +0200

    Cleanup: Should use vs2017 - Appveyor configuration - build.bat more in line with new solution format - Removing nuspec and moved information to fsproj

--- build.bat
+++ build.bat
@@ -1,22 +1,12 @@
 @echo off
 
 cls
-if %PROCESSOR_ARCHITECTURE%==x86 (
-         set MSBUILD="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-) else ( set MSBUILD="%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
-)
-if not exist .nuget\nuget.exe %MSBUILD% .nuget\nuget.targets /t:CheckPrerequisites
-%MSBUILD% /m /p:Configuration=SystemJson Fleece.sln
+msbuild /m /t:restore Fleece.sln
+msbuild /m  Fleece.sln
 if ERRORLEVEL 1 (
-	echo Error building Fleece for System.Json
+	echo Error building Fleece
 	exit /b 1
 ) else (
-	.nuget\nuget.exe pack Fleece.nuspec
-)
-%MSBUILD% /m /p:Configuration=FSharpData Fleece.sln
-if ERRORLEVEL 1 (
-	echo Error building Fleece for FSharp.Data
-	exit /b 2
-) else (
-	.nuget\nuget.exe pack Fleece.FSharpData.nuspec
+	dotnet pack Fleece
+	dotnet pack Fleece.FSharpData
 )
