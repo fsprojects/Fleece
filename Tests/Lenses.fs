b82d14596c7c2c8ebeaa8c1d11041d7202479ab7
Oskar Gewalli <oskar@gewalli.se>
2018-07-23 18:08:17 +0300

    write with missing key doesn't work

--- Tests/Lenses.fs
+++ Tests/Lenses.fs
@@ -19,7 +19,6 @@ open System.Json
 open Newtonsoft.Json
 open Newtonsoft.Json.Linq
 #endif
-
 let tests = [
         testList "key" [
             test "example 1: read first key" {
@@ -36,10 +35,22 @@ let tests = [
                 let actual = JsonValue.Parse( "{\"a\": true, \"b\": 200}" ) ^? (_key "c" << _Number)
                 Assert.Equal("item", None, actual)
             }
-            test "example 3" {
+            test "example 4: write with missing key" { //TODO: Fix
+                let actual = JsonValue.Parse( "{\"a\": true, \"b\": 200}" )|> (_key "c" << _String) .-> "a"
+                let expected = JsonValue.Parse ("{\"a\": true, \"b\": 200, \"c\":\"a\"}")
+                //Assert.Equal("item", expected.ToString(), actual.ToString())
+                printfn "todo: %A ~ %A" expected actual
+            }
+            test "example 5: write existing key" {
+                let actual = JsonValue.Parse( "{\"a\": true, \"b\": 200}" )|> (_key "a" << _Bool) .-> false
+                let expected = JsonValue.Parse ("{\"a\": false, \"b\": 200}")
+                Assert.Equal("item", expected.ToString(), actual.ToString())
+            }
+            test "example 6: read key from a different type" {
                 let actual = JsonValue.Parse( "[1,2,3]" ) ^? _key "a"
                 Assert.Equal("item", None, actual)
             }
+
         ]
         testList "_String" [
             test "example 1" {
@@ -50,6 +61,11 @@ let tests = [
             test "example 2" {
                 let actual = JsonValue.Parse ("{\"a\": \"xyz\", \"b\": true}") ^? (_key "b" << _String)
                 Assert.Equal("item", None, actual)
+            }
+            test "example 3" {
+                let actual = JsonValue.Parse ("\"a\"") |>  _String .-> "b"
+                let expected = JsonValue.Parse( "\"b\"" )
+                Assert.Equal("item", expected.ToString(), actual.ToString())
             }
         ]
         testList "Integer" [
