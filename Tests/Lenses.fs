7a72ec7c20a18c3bfd49010ca9b87dcaab6f6e1d
Oskar Gewalli <oskar@gewalli.se>
2018-07-23 12:55:17 +0300

    Adding additional tests

--- Tests/Lenses.fs
+++ Tests/Lenses.fs
@@ -23,15 +23,19 @@ open Newtonsoft.Json.Linq
 let tests = 
     TestList [
         testList "key" [
-            test "example 1" {
+            test "example 1: read first key" {
                 let actual = JsonValue.Parse( "{\"a\": true, \"b\": 200}" ) ^? (_key "a" << _Bool)
                 let expected = true
                 Assert.Equal("item", Some expected, actual)
             }
-            test "example 2" {
+            test "example 2: read second key" {
                 let actual = JsonValue.Parse( "{\"a\": true, \"b\": 200}" ) ^? (_key "b" << _Number)
                 let expected = 200m
                 Assert.Equal("item", Some expected, actual)
+            }
+            test "example 3: read with missing key" {
+                let actual = JsonValue.Parse( "{\"a\": true, \"b\": 200}" ) ^? (_key "c" << _Number)
+                Assert.Equal("item", None, actual)
             }
             test "example 3" {
                 let actual = JsonValue.Parse( "[1,2,3]" ) ^? _key "a"
@@ -67,6 +71,21 @@ let tests =
                 let expected = 123m
                 Assert.Equal("item", Some expected, actual)
             }
+            test "example 3: read for missing index" {
+                let actual = JsonValue.Parse ("[1,2,3]") ^? (_nth 4 << _Number)
+                Assert.Equal("item", None, actual)
+            }
+            test "example 4: write" {
+                let actual = JsonValue.Parse ("[1,2,3]") |> (_nth 1) .-> JString "a"
+                let expected = JsonValue.Parse ("[1,\"a\",3]")
+                Assert.Equal("item", expected, actual)
+            }
+            test "example 5: write for missing index" {
+                let actual = JsonValue.Parse ("[1]") |> (_nth 1) .-> JString "a"
+                let expected = JsonValue.Parse ("[1]")
+                Assert.Equal("item", expected, actual)
+            }
+
         ]
 
     ]
