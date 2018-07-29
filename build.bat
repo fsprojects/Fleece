02ac4c362fe29833561adeb3104412f3932035a8
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2018-07-29 11:11:50 +0200

    Include all dlls in build.bat

--- build.bat
+++ build.bat
@@ -2,11 +2,27 @@
 
 cls
 msbuild /m /t:restore Fleece.sln
+if ERRORLEVEL 1 (
+	echo Error restoring packages
+	exit /b 1
+) 
 msbuild /m  Fleece.sln
 if ERRORLEVEL 1 (
 	echo Error building Fleece
 	exit /b 1
-) else (
-	dotnet pack Fleece
-	dotnet pack Fleece.FSharpData
+) 
+dotnet pack Fleece
+if ERRORLEVEL 1 (
+	echo Error creating package for Fleece
+	exit /b 1
+)
+dotnet pack Fleece.FSharpData
+if ERRORLEVEL 1 (
+	echo Error creating package for Fleece.FSharpData
+	exit /b 1
+)
+dotnet pack Fleece.NewtonsoftJson
+if ERRORLEVEL 1 (
+	echo Error creating package for Fleece.NewtonsoftJson
+	exit /b 1
 )
