c8102d203762bc303e5b70deb5530aa9d81daf7c
Oskar Gewalli <oskar@gewalli.se>
2018-08-22 14:33:25 +0200

    Adding sample code

--- IntegrationCompilationTests/Library.fs
+++ IntegrationCompilationTests/Library.fs
@@ -1,4 +1,37 @@
 namespace IntegrationCompilationTests
+open FSharp.Data
+open Fleece
+open Fleece.Operators
+open System.Json
+open Newtonsoft.Json
+open FSharpPlus
+
+type Person = {
+    Name: string
+    Age: int
+    Children: Person list
+}
+
+type Person with
+    static member Create name age children = { Person.Name = name; Age = age; Children = children }
+
+    static member OfJson json = 
+        match json with
+        | JObject o -> Person.Create <!> (o .@ "name") <*> (o .@ "age") <*> (o .@ "children")
+        | x -> Failure (sprintf "Expected person, found %A" x)
+
+    static member ToJson (x: Person) =
+        jobj [ 
+            "name" .= x.Name
+            "age" .= x.Age
+            "children" .= x.Children
+        ] 
+
+type Attribute = {
+    Name: string
+    Value: string
+}
+
 
 module Say =
     let hello name =
