b6324fa50b55cc556a6921b5f0f57de91e3f6593
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2018-08-28 23:13:16 +0200

    Benchmarks from haf/chiron/chiron-7 (#35)
    * First copy of benchmarks from haf/chiron/chiron-7
    
    * Changing social json format

---
+++ Benchmarks/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+module FleeceB.Benchmarks.Program
+
+open BenchmarkDotNet.Configs
+open BenchmarkDotNet.Analysers
+open BenchmarkDotNet.Diagnosers
+//open BenchmarkDotNet.Diagnostics.Windows
+open BenchmarkDotNet.Validators
+open BenchmarkDotNet.Running
+
+[<EntryPoint>]
+let main argv =
+    let switcher = BenchmarkSwitcher thisAssembly
+    let _ = switcher.Run argv
+    0
