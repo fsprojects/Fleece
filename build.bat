442c6e4b5282bb8d19a1232e0424118f01635523
Enrico Sada <enrico.sada@jet.com>
2018-09-06 16:33:26 +0200

    Default of dotnet pack is Debug configuration, use Release

--- build.bat
+++ build.bat
@@ -11,17 +11,20 @@ if ERRORLEVEL 1 (
 	echo Error building Fleece
 	exit /b 1
 ) 
-dotnet pack Fleece
+
+dotnet pack Fleece -c Release
 if ERRORLEVEL 1 (
 	echo Error creating package for Fleece
 	exit /b 1
 )
-dotnet pack Fleece.FSharpData
+
+dotnet pack Fleece.FSharpData -c Release
 if ERRORLEVEL 1 (
 	echo Error creating package for Fleece.FSharpData
 	exit /b 1
 )
-dotnet pack Fleece.NewtonsoftJson
+
+dotnet pack Fleece.NewtonsoftJson -c Release
 if ERRORLEVEL 1 (
 	echo Error creating package for Fleece.NewtonsoftJson
 	exit /b 1
