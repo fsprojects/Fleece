e2e714d2fea8ad925f1ee25c56f19c39509d2a9e
Oskar Gewalli <oskar@gewalli.se>
2018-09-17 18:28:51 +0200

    The type 'JsonValue' does not have 'null' as a proper value

--- Fleece/Fleece.fs
+++ Fleece/Fleece.fs
@@ -920,7 +920,7 @@ module Fleece =
         let inline _JArray  x = (prism' JArray  <| fun v -> match v with JArray  s -> Some s | _ -> None) x
         let inline _JBool   x = (prism' JBool   <| fun v -> match v with JBool   s -> Some s | _ -> None) x
         let inline _JNumber x = (prism' JNumber <| fun v -> match ofJson v : decimal ParseResult with Ok s->Some s | _ -> None) x
-        let inline _JNull   x = prism' (konst null) (fun v -> match v with JNull -> Some () | _ -> None) x
+        let inline _JNull   x = prism' (konst JNull) (fun v -> match v with JNull -> Some () | _ -> None) x
         /// Like '_jnth', but for 'Object' with Text indices.
         let inline _jkey i =
             let inline dkey i f t = map (fun x -> IReadOnlyDictionary.add i x t) (f (IReadOnlyDictionary.tryGetValue i t |> Option.defaultValue JNull))
