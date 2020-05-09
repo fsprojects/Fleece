0b5711b5485355cb00ed2db28a2ffdeeb8ff1cba
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-05-09 17:35:50 +0200

    Generate docs

--- build.bat
+++ build.bat
@@ -7,19 +7,19 @@ if ERRORLEVEL 1 (
 	exit /b 1
 ) 
 
-dotnet pack Fleece.SystemJson -c Release
+dotnet pack src\Fleece.SystemJson -c Release
 if ERRORLEVEL 1 (
 	echo Error creating package for Fleece.SystemJson
 	exit /b 1
 )
 
-dotnet pack Fleece.FSharpData -c Release
+dotnet pack src\Fleece.FSharpData -c Release
 if ERRORLEVEL 1 (
 	echo Error creating package for Fleece.FSharpData
 	exit /b 1
 )
 
-dotnet pack Fleece.NewtonsoftJson -c Release
+dotnet pack src\Fleece.NewtonsoftJson -c Release
 if ERRORLEVEL 1 (
 	echo Error creating package for Fleece.NewtonsoftJson
 	exit /b 1
--- docsrc/tool/manually_build_docs.sh
+++ docsrc/tool/manually_build_docs.sh
@@ -5,8 +5,9 @@ cd ../../
 dotnet tool restore
 
 # Build
-dotnet build -c Release
+# dotnet build -c Release
+msbuild /t:Build /p:Configuration=Release 
 
 # Gen docs
-dotnet run --project ./docsrc/tools
+dotnet run --project ./docsrc/tool
 
