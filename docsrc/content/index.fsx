19915110548b7a6ca4437dd09bbf110cb2d5cf43
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-06-21 20:53:55 +0200

    +index text

--- docsrc/content/index.fsx
+++ docsrc/content/index.fsx
@@ -5,14 +5,18 @@
 (**
 Fleece
 ======
+
+Fleece is a library intended to help with parsing and crafting specific Json without having to write data transfer objects in order to get the expected 
+representation.
+
 *)
 
 (**
 
 ### Introduction
 
- - [ToJson and OfJson](./to-json-and-of-json.html)
- - [Codec](./codec.html)
- - [Combinators](./combinators.html)
+ - [ToJson and OfJson](./to-json-and-of-json.html) are the basic building blocks
+ - [Codec](./codec.html) let's you combine both ToJson and OfJson in one declaration
+ - [Combinators](./combinators.html) lets you have more control
 
 *)
