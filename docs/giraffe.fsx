2bc272e204744723b434f18eb49d8bb87ac97f12
Ross Donaldson <gastove@gmail.com>
2021-02-21 14:06:50 -0800

    Allow new docs in gitignore, track

---
+++ docs/giraffe.fsx
@@ -0,0 +1,91 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use
+// it to define helpers that you do not want to show in the documentation.
+#r "nuget: FSharpPlus"
+#r "nuget: System.Json"
+#r "nuget: TaskBuilder.fs"
+#r @"../src/Fleece.SystemJson/bin/Release/netstandard2.1/Fleece.SystemJson.dll"
+
+module Giraffe=
+    open System.Threading.Tasks
+    open System.IO
+    /// fake definition
+    type Request()=
+        member __.Body:Stream = failwith "not implemented"
+    type HttpContext ()=
+        member __.Request : Request=failwith "not implemented"
+        member __.SetContentType(s:string)=failwith "not implemented"
+        member __.WriteBytesAsync(b:byte array) : Task<HttpContext option> =failwith "not implemented"
+
+    type HttpFuncResult = Task<HttpContext option>
+
+    type HttpFunc = HttpContext -> HttpFuncResult
+
+    type HttpHandler = HttpFunc -> HttpFunc
+
+
+(**
+## Giraffe
+
+In this page we will get an overview of how you can use Fleece together with Giraffe.
+
+A minimal integration can be done by looking at how Giraffe implements the method [WriteJsonAsync](https://github.com/giraffe-fsharp/Giraffe/blob/37e69a54d1e85649968705f13cab77abe2d0a928/src/Giraffe/ResponseWriters.fs#L53-L57) and function [json](https://github.com/giraffe-fsharp/Giraffe/blob/37e69a54d1e85649968705f13cab77abe2d0a928/src/Giraffe/ResponseWriters.fs#L186-L188):
+
+*)
+
+open Giraffe
+open System.IO
+open System.Text
+// task computation builder from TaskBuilder.fs:
+open FSharp.Control.Tasks.V2.ContextInsensitive
+// Fleece and Json related:
+open Fleece.SystemJson
+open Fleece.SystemJson.Operators
+
+module BusinessApp=
+    module Json =
+        let inline json (dataObj ) : HttpHandler =
+            fun (_ : HttpFunc) (ctx : HttpContext) ->
+                ctx.SetContentType "application/json; charset=utf-8"
+                toJson dataObj // turn dataObj into Json
+                |> string // get the Json string
+                |> Encoding.UTF8.GetBytes // turn the string into bytes
+                |> ctx.WriteBytesAsync // write bytes to the response
+
+        let inline bindJsonAsync (ctx : HttpContext) =
+            task {
+                use reader = new StreamReader(ctx.Request.Body)
+                let! body = reader.ReadToEndAsync()
+                return parseJson body
+            }
+
+(**
+In the web API part of your business app you would then do something like the code below:
+*)
+
+open Giraffe
+open FSharp.Control.Tasks.V2.ContextInsensitive
+// we open the Json helpers we defined last in order to avoid using the default "json" function from Giraffe:
+open BusinessApp.Json
+type Person = { Name : string }
+with
+    static member JsonObjCodec =
+        fun name -> { Name = name }
+        <!> jreq  "name" (Some << fun x -> x.Name)
+
+let personHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            match! bindJsonAsync ctx with // instead of using ctx.BindJsonAsync we use the function above
+            | Ok (person:Person)->
+                return! json person next ctx
+            // and ideally we would deal with case when the parsing fails as well
+        }
+
+(**
+The benefit of doing an integration in this way is:
+
+- You get a compilation error when trying to use types that don't have the proper functions defined.
+- You avoid having to use runtime reflection to bind and serialize the Json.
+- You have more control over the serialization than with System.Text.Json or Newtonsoft.Json
+*)
---
+++ docs/to-json-and-of-json.fsx
@@ -0,0 +1,148 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use
+// it to define helpers that you do not want to show in the documentation.
+#r "nuget: FSharpPlus"
+#r "nuget: System.Json"
+#r @"../src/Fleece.SystemJson/bin/Release/netstandard2.1/Fleece.SystemJson.dll"
+
+open System.Json
+open Fleece.SystemJson
+open Fleece.SystemJson.Operators
+#if FSHARPDATA
+#r "nuget: FSharp.Data"
+#r @"../../src/Fleece.FSharpData/bin/Release/netstandard2.1/Fleece.FSharpData.dll"
+
+open FSharp.Data
+open Fleece.FSharpData
+open Fleece.FSharpData.Operators
+#endif
+(**
+## ToJson and OfJson
+
+In order to parse or encode instances into Json you can define static members
+
+For example, given this data type:
+*)
+
+
+type Person = {
+    Name: string
+    Age: int
+    Children: Person list
+}
+
+(**
+You can map it to JSON like this:
+*)
+
+
+type Person with
+    static member ToJson (x: Person) =
+        jobj [
+            "name" .= x.Name
+            "age" .= x.Age
+            "children" .= x.Children
+        ]
+
+let p =
+    { Person.Name = "John"
+      Age = 44
+      Children =
+      [
+        { Person.Name = "Katy"
+          Age = 5
+          Children = [] }
+        { Person.Name = "Johnny"
+          Age = 7
+          Children = [] }
+      ] }
+
+printfn "%s" (string (toJson p))
+
+(**
+And you can map it from JSON like this:
+*)
+
+type Person with
+    static member OfJson json =
+        match json with
+        | JObject o ->
+            let name = o .@ "name"
+            let age = o .@ "age"
+            let children = o .@ "children"
+            match name, age, children with
+            | Decode.Success name, Decode.Success age, Decode.Success children ->
+                Decode.Success {
+                    Person.Name = name
+                    Age = age
+                    Children = children
+                }
+            | x -> Error <| Uncategorized (sprintf "Error parsing person: %A" x)
+        | x -> Decode.Fail.objExpected x
+
+let john : Person ParseResult = parseJson """{
+    "name": "John",
+    "age": 44,
+    "children": [{
+        "name": "Katy",
+        "age": 5,
+        "children": []
+    }, {
+        "name": "Johnny",
+        "age": 7,
+        "children": []
+    }]
+}"""
+
+(**
+Though it's much easier to do this in a monadic or applicative way. For example, using [FSharpPlus](https://github.com/fsprojects/FSharpPlus) (which is already a dependency of Fleece):
+*)
+
+open FSharpPlus
+
+type PersonAp = {
+    Name: string
+    Age: int
+    Children: PersonAp list
+}
+
+type PersonAp with
+    static member Create name age children = { PersonAp.Name = name; Age = age; Children = children }
+
+    static member OfJson json =
+        match json with
+        | JObject o -> PersonAp.Create <!> (o .@ "name") <*> (o .@ "age") <*> (o .@ "children")
+        | x -> Decode.Fail.objExpected x
+
+(**
+
+Or monadically:
+*)
+
+type PersonM = {
+    Name: string
+    Age: int
+    Children: PersonM list
+}
+
+type PersonM with
+    static member OfJson json =
+        match json with
+        | JObject o ->
+            monad {
+                let! name = o .@ "name"
+                let! age = o .@ "age"
+                let! children = o .@ "children"
+                return {
+                    Person.Name = name
+                    Age = age
+                    Children = children
+                }
+            }
+        | x -> Decode.Fail.objExpected x
+
+(**
+Or you can use the Choice monad/applicative in [FSharpx.Extras](https://github.com/fsprojects/FSharpx.Extras) instead, if you prefer.
+
+You can see more examples in the [EdmundsNet](https://github.com/mausch/EdmundsNet) project.
+*)
