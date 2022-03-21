787f9809390c1c70335bcd193ad3ab3dd41b0fbd
Gusty <1261319+gusty@users.noreply.github.com>
2022-03-21 21:41:14 +0100

    Fix prev: duplicate call to avoid internal error

--- src/Fleece/Fleece.fs
+++ src/Fleece/Fleece.fs
@@ -815,7 +815,8 @@ module Internals =
         // Overload for tuples
         static member inline GetCodec (_:'tuple when 'Encoding :> IEncoding and 'Encoding : (new : unit -> 'Encoding), _: obj, c, _: 'Operation) : Codec<'Encoding, _> =
             let _f t = (^tuple: (member Item1: 't1) t)
-            GetArrCodec.Invoke Unchecked.defaultof<'tuple> >.> Codecs.array Codecs.id
+            let inline call (a: ^a, b: ^b) = ((^a or ^b) : (static member GetArrCodec: ^b * ^a * ^a * _ -> Codec<'Encoding [], ^t>) b, a, a, Unchecked.defaultof<'Operation>)
+            call (Unchecked.defaultof<GetArrCodec>, Unchecked.defaultof<'tuple>) >.> Codecs.array Codecs.id
 
         // For specific 'Encoding in recursive calls coming from a get_Codec operation
         static member inline GetCodec (_: 'T, _: IDefault7, _, _: 'Operation) : Codec<'Encoding, 'T> =
